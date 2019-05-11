a8d63d530cc70fca348b963c6103b1b4d9466bfe
dealkmin <deborah_alkmin@hotmail.com>
2019-05-11 00:09:06 -0300

    Inicio

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CarouselComponent } from './carousel/carousel.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CarouselComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
