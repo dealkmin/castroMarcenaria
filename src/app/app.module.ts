d3b40bc47a23da169ad823e8f7d3e5c8dd5c39e1
Rafael Prado <rafaelpradodelima@gmail.com>
2019-05-24 06:59:49 -0300

    fix: ng-bootstrap fix
    Signed-off-by: Rafael Prado <rafaelpradodelima@gmail.com>
    
    Took 2 minutes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 import { RootComponent } from './root/root.component';
 import { HeaderComponent } from './header/header.component';
@@ -7,18 +8,8 @@ import { CarouselComponent } from './carousel/carousel.component';
 
 @NgModule({
   declarations: [RootComponent, HeaderComponent, CarouselComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, NgbModule],
   providers: [],
   bootstrap: [RootComponent]
 })
 export class AppModule {}
-
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
-
-@NgModule({
-  ...
-  imports: [NgbModule, ...],
-  ...
-})
-export class YourAppModule {
-}
