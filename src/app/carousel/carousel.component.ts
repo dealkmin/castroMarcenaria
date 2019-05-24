e9a8315b97a786b97a93861a7e96c85b2aa31729
Rafael Prado <rafaelpradodelima@gmail.com>
2019-05-24 06:56:42 -0300

    feat: add instagram's images feat: add working carousel with minimum style
    Signed-off-by: Rafael Prado <rafaelpradodelima@gmail.com>
    
    Took 49 minutes

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -6,6 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./carousel.component.css']
 })
 export class CarouselComponent implements OnInit {
+  images = [
+    'image_1.jpg',
+    'image_2.jpg',
+    'image_3.jpg',
+    'image_4.jpg',
+    'image_5.jpg',
+    'image_6.jpg'
+  ];
+
   constructor() {}
 
   ngOnInit() {}
