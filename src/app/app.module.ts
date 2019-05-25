71f880fc5b2ef12389b233f0ef36bda55cf630d5
dealkmin <deborah_alkmin@hotmail.com>
2019-05-25 16:48:49 -0300

    FE-2 Adiciona Btn Orçamento

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,11 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RootComponent } from './root/root.component';
 import { HeaderComponent } from './header/header.component';
 import { CarouselComponent } from './carousel/carousel.component';
+import { BtnOrcamentoIniciarComponent } from './btn-orcamento-iniciar/btn-orcamento-iniciar.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @NgModule({
-  declarations: [RootComponent, HeaderComponent, CarouselComponent],
+  declarations: [RootComponent, HeaderComponent, CarouselComponent, BtnOrcamentoIniciarComponent, TestimonialsComponent],
   imports: [BrowserModule, NgbModule],
   providers: [],
   bootstrap: [RootComponent]
